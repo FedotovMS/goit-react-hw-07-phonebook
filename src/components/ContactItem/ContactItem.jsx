3be5635b063c5810f44184c3a027bdcedda25f30
Mykola Fedotov <fedotovns@gmail.com>
2023-06-14 21:16:04 +0200

    finish

---
+++ src/components/ContactItem/ContactItem.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
+import { deleteContact } from 'redux/operations';
+import css from './ContactItem.module.css';
+
+const ContactItem = ({ contact }) => {
+  const dispatch = useDispatch();
+
+  return (
+    <li className={css.list}>
+      {contact.name}: {contact.phone}
+      <button
+        className={css.button}
+        onClick={() => dispatch(deleteContact(contact.id))}
+      >
+        Delete
+      </button>
+    </li>
+  );
+};
+
+ContactItem.propTypes = {
+  contact: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    number: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default ContactItem;
